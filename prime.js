b8b2a14b9191e85615573b0cf600efde1baa5872
Akashpatel2606 <factsamotivation@gmail.com>
2022-12-04 10:55:56 +0530

    prime_complete

---
+++ prime.js
@@ -0,0 +1,13 @@
+let num=7;
+let p=0;
+   for(let i=1;i<=num;i++){
+       if(num%i==0){
+         p+=1;
+   }
+  }
+       if(p==2){
+          console.log("prime")
+       }
+        else{
+          console.log("not prime")
+}
